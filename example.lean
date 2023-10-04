f182262b9752862a3bfca11470d652d2087366c7
Luis Ángel Méndez Gort <lamg@protonmail.com>
2023-10-04 16:34:05 +0200

    some lean and sieve

---
+++ example.lean
@@ -0,0 +1,19 @@
+#eval 1+3
+
+#eval String.append "hello" " world"
+
+def hello := "hello"
+
+#eval hello
+
+def add1 (x : Nat) : Nat := x + 1
+
+#eval add1 10
+
+structure Point where
+  x : Nat
+  y : Nat
+
+#check ({ x := 10, y := 20 }:Point)
+
+#check Nat -> Nat
---
+++ sieve.fsx
@@ -0,0 +1,6 @@
+let ns = Seq.initInfinite id |> Seq.skip 2
+let isPrime n = Seq.forall (fun x -> n % x <> 0) {2..n-1}
+let primes  = ns |> Seq.filter isPrime
+let firstNPrimes n = primes |> Seq.take n |> Seq.toList
+
+printfn "the first 10 primes = %A" (firstNPrimes 10)
