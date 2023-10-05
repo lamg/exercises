040f5181d040c95a51e80029adf45481b8e5dfea
Luis Ángel Méndez Gort <lamg@protonmail.com>
2023-10-05 16:20:42 +0200

    running with more data

--- prime_generator/run.sh
+++ prime_generator/run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-time cat data.txt | dotnet fsi -O prime_generator.fsx > primes.txt
+echo 'running for spoj_example.txt …'
+time cat spoj_example.txt | dotnet fsi -O prime_generator.fsx > spoj_example.output.txt
+echo ''
+echo 'running for data.txt …'
+time cat data.txt | dotnet fsi -O prime_generator.fsx > data.output.txt
