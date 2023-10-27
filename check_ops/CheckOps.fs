600cb2689b386860f6dea68ca7c5c3d810d8f9d2
Luis Ángel Méndez Gort <lamg@protonmail.com>
2023-10-27 17:04:23 +0200

    no file system interaction

--- check_ops/CheckOps.fs
+++ check_ops/CheckOps.fs
@@ -3,6 +3,7 @@
 // check an F# file holds the following properties
 // - contains a function with the signature `main: string seq -> string seq`
 // - does not write or read from stdin, stdout, stderr
+// - does not interact with the file system
 // - does not load any external libraries
 
 open FSharp.Analyzers.SDK
