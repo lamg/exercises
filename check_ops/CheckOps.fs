609d55590019e1fd5c6df8fad54a9bbb14a4f0a6
Luis Ángel Méndez Gort <lamg@protonmail.com>
2023-10-27 16:52:49 +0200

    check_ops

---
+++ check_ops/CheckOps.fs
@@ -0,0 +1,22 @@
+﻿module CheckOps
+
+// check an F# file holds the following properties
+// - contains a function with the signature `main: string seq -> string seq`
+// - does not write or read from stdin, stdout, stderr
+// - does not load any external libraries
+
+open FSharp.Analyzers.SDK
+
+let analyzer (ctx: CliContext) : Async<Message list> =
+        async {
+            return
+                [ { Type = "CheckOps analyzer"
+                    Message = "Forbidden operations are used in this file"
+                    Code = "CO001"
+                    Severity = Error
+                    Range = FSharp.Compiler.Text.Range.Zero
+                    Fixes = [] } ]
+        }
+
+[<CliAnalyzer "CheckOps">]
+let main: Analyzer<CliContext> = analyzer
