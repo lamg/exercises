179f3e34880f2830878fb351cf8ab24990922c7f
Luis Ángel Méndez Gort <lamg@protonmail.com>
2023-11-01 15:24:48 +0100

    no output directory

--- convert/convert.fish
+++ convert/convert.fish
@@ -1,10 +1,7 @@
 #!/usr/bin/env fish
 
-# create output directory if it doesn't exist
-mkdir -p output
-
 # loop through all webm files in directory
 for file in *.webm
     # extract audio from webm file and save as opus file
-    ffmpeg -i "$file" -vn -acodec copy "output/$file:r.opus"
+    ffmpeg -i "$file" -vn -acodec copy "$file.opus"
 end
