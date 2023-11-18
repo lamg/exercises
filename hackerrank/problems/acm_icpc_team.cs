24d3a684eaf9a5967c2975983eb006e640d9a349
Luis Ángel Méndez Gort <lamg@protonmail.com>
2023-11-18 17:27:13 +0100

    ACM ICPC team

---
+++ hackerrank/problems/acm_icpc_team.cs
@@ -0,0 +1,41 @@
+﻿namespace Problems;
+
+public partial class Problems
+{
+
+    static int GetTopicCount(string a, string b)
+    {
+        var count = 0;
+        for (var i = 0; i < a.Length; i++)
+        {
+            if (a[i] == '1' || b[i] == '1')
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static List<int> acmTeam(List<string> topic)
+    {
+        var max = 0;
+        var total = 0;
+        for (var i = 0; i < topic.Count; i++)
+        {
+            for (var j = i + 1; j < topic.Count; j++)
+            {
+                var count = GetTopicCount(topic[i], topic[j]);
+                if (count > max)
+                {
+                    max = count;
+                    total = 1;
+                }
+                else if (count == max)
+                {
+                    total++;
+                }
+            }
+        }
+        return new List<int> { max, total };
+    }
+}
