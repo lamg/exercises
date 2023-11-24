7a6edd5f8fb2576af4502bd8127f3772e39187b9
Luis Ángel Méndez Gort <lamg@protonmail.com>
2023-11-24 15:38:04 +0100

    Gtk# alive and kicking

---
+++ gtk_sharp_experiment/Program.fs
@@ -0,0 +1,15 @@
+﻿open Gtk
+
+[<EntryPoint>]
+let main argv =
+    Application.Init()
+
+    let app = new Application("org.gtk_sharp_experiment.gtk_sharp_experiment", GLib.ApplicationFlags.None)
+    app.Register(GLib.Cancellable.Current) |> ignore;
+
+    let win = new Main.MainWindow()
+    app.AddWindow(win)
+
+    win.Show()
+    Application.Run()
+    0
