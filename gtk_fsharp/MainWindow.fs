4f1a49326ced256f839b0a1202fbc806a73053c4
Luis Ángel Méndez Gort <lamg@protonmail.com>
2024-04-13 19:27:09 +0200

    experiment with F# and Gtk#

---
+++ gtk_fsharp/MainWindow.fs
@@ -0,0 +1,25 @@
+module Main
+
+open Gtk
+
+type MainWindow (builder: Builder, baseBuilder: nativeint) =
+  inherit Window(baseBuilder)
+
+let newMainWindow () =
+  let builder = new Builder("MainWindow.glade")
+  let window = builder.GetRawOwnedObject("MainWindow")
+  
+  let m = new MainWindow(builder, window)
+  builder.Autoconnect m
+  m.DeleteEvent.Add (fun _ -> Application.Quit())
+  
+  let mutable _label1 = builder.GetObject "_label1" :?> Label
+
+  let mutable _button1 = builder.GetObject "_button1" :?> Button
+
+  let mutable _counter = 0
+  _button1.Clicked.Add (fun _ -> 
+    _counter <- _counter + 1
+    _label1.Text <- $"This button has been clicked {_counter}"
+  )
+  m
---
+++ gtk_fsharp/Program.fs
@@ -0,0 +1,13 @@
+open System
+open Gtk
+
+[<EntryPoint>]
+let main args =
+    Application.Init()
+    let app = new Application("org.interacto.interacto", GLib.ApplicationFlags.None)
+    app.Register(GLib.Cancellable.Current) |> ignore
+    let win = Main.newMainWindow()
+    app.AddWindow(win)
+    win.Show()
+    Application.Run()
+    0
