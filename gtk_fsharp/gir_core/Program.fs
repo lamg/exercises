666ddf665a5a490ecc7bbd6ed9cc95abef339ea8
Luis Ángel Méndez Gort <lamg@protonmail.com>
2024-06-02 11:02:26 +0200

    basic window with F# and gir_core

---
+++ gtk_fsharp/gir_core/Program.fs
@@ -0,0 +1,16 @@
+﻿[<EntryPoint>]
+let main _ =
+  let application =
+    Gtk.Application.New("org.gir.core", Gio.ApplicationFlags.FlagsNone)
+
+  let f (sender: Gio.Application) (_: System.EventArgs) =
+    let window = Gtk.ApplicationWindow.New(sender :?> Gtk.Application)
+    window.Title <- "Gtk4 Window"
+    window.SetDefaultSize(300, 300)
+    window.Show()
+
+  let g = new GObject.SignalHandler<Gio.Application>(f)
+
+  application.add_OnActivate g
+
+  application.RunWithSynchronizationContext(null)
