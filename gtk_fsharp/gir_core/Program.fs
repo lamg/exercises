2ec8e7345fe9129bda068d13ac83be7719e9a1e6
Luis Ángel Méndez Gort <lamg@protonmail.com>
2024-06-02 12:16:05 +0200

    some widgets added

--- gtk_fsharp/gir_core/Program.fs
+++ gtk_fsharp/gir_core/Program.fs
@@ -1,16 +1,43 @@
-﻿[<EntryPoint>]
+﻿open Gtk
+
+let label () =
+  let label = new Label()
+  label.SetText "hello"
+  label
+
+let button (label: Label) =
+  let button = new Button()
+  button.SetLabel "click me"
+  let mutable counter = 0
+
+  let clickHnd (_: Button) (_: System.EventArgs) =
+    label.SetText $"hello {counter}"
+    counter <- counter + 1
+
+  button.add_OnClicked (new GObject.SignalHandler<Button>(clickHnd))
+  button
+
+let box () =
+  let box = new Box()
+  box.SetOrientation Orientation.Vertical
+  box.SetHomogeneous true
+
+  let l = label ()
+  box.Append l
+  button l |> box.Append
+  box
+
+let handler (sender: Gio.Application) (_: System.EventArgs) =
+  let window = ApplicationWindow.New(sender :?> Application)
+  window.Title <- "Gtk4 Window"
+  window.SetDefaultSize(300, 300)
+  window.SetChild(box ())
+  window.Show()
+
+[<EntryPoint>]
 let main _ =
-  let application =
-    Gtk.Application.New("org.gir.core", Gio.ApplicationFlags.FlagsNone)
+  let application = Application.New("org.gir.core", Gio.ApplicationFlags.FlagsNone)
 
-  let f (sender: Gio.Application) (_: System.EventArgs) =
-    let window = Gtk.ApplicationWindow.New(sender :?> Gtk.Application)
-    window.Title <- "Gtk4 Window"
-    window.SetDefaultSize(300, 300)
-    window.Show()
-
-  let g = new GObject.SignalHandler<Gio.Application>(f)
-
-  application.add_OnActivate g
+  application.add_OnActivate (new GObject.SignalHandler<Gio.Application>(handler))
 
   application.RunWithSynchronizationContext(null)
