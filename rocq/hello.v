25512537a8b6f9233bfba643b69776d5dd046fea
Luis Ángel Méndez Gort <lamg@protonmail.com>
2025-07-15 13:11:54 +0200

    andb3 exercise

--- rocq/hello.v
+++ rocq/hello.v
@@ -85,10 +85,21 @@ Proof. reflexivity. Qed.
 Definition nandb (b1:bool) (b2:bool) : bool
   := negb (andb b1 b2).
 Example test_nandb1: (nandb true false) = true.
-Proof reflexivity.
+Proof. reflexivity. Qed.
 Example test_nandb2: (nandb false false) = true.
-(* FILL IN HERE *) Admitted.
+Proof. reflexivity. Qed.
 Example test_nandb3: (nandb false true) = true.
-(* FILL IN HERE *) Admitted.
+Proof. reflexivity. Qed.
 Example test_nandb4: (nandb true true) = false.
-(* FILL IN HERE *) Admitted.
+Proof. reflexivity. Qed.
+
+Definition andb3 (b1:bool) (b2:bool) (b3:bool) : bool :=
+  andb b1 (andb b2 b3).
+Example test_andb31: (andb3 true true true) = true.
+Proof. reflexivity. Qed.
+Example test_andb32: (andb3 false true true) = false.
+Proof. reflexivity. Qed.
+Example test_andb33: (andb3 true false true) = false.
+Proof. reflexivity. Qed.
+Example test_andb34: (andb3 true true false) = false.
+Proof. reflexivity. Qed.
