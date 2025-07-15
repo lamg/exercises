b6b1966d082c6aa829955a7812939ba553bafe03
Luis Ángel Méndez Gort <lamg@protonmail.com>
2025-07-15 12:51:01 +0200

    the two negs are equivalent!

--- rocq/hello.v
+++ rocq/hello.v
@@ -61,5 +61,12 @@ Example or_notation:
 Proof. simpl. reflexivity. Qed.
 
 Example and_notation:
-  true && false = true.
+  true && false = false.
+Proof. simpl. reflexivity. Qed.
+
+Definition negb' (b: bool): bool :=
+  if b then false else true.
+
+Example negs_are_equal:
+  negb = negb'.
 Proof. simpl. reflexivity. Qed.
