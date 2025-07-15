8eb9b8848284b39118386a77ccc26a8e02d19705
Luis Ángel Méndez Gort <lamg@protonmail.com>
2025-07-15 12:12:43 +0200

    negb and andb

--- rocq/hello.v
+++ rocq/hello.v
@@ -24,3 +24,21 @@ Example test_next_working_day:
   (next_working_day (next_working_day saturday)) = tuesday.
 Proof. simpl. reflexivity. Qed.
 
+Inductive bool: Type :=
+  | true
+  | false.
+
+Definition negb (b: bool): bool :=
+  match b with
+  | true => false
+  | false => true
+  end.
+
+Definition andb (x: bool) (y: bool): bool :=
+  match x with
+  | true => y
+  | false => false
+  end.
+
+Compute (andb (negb true) false).
+
