13eef90cf77afe8857146f6c830530999d4d12b7
Luis Ángel Méndez Gort <lamg@protonmail.com>
2025-07-15 12:04:51 +0200

    first proof in Rocq

---
+++ rocq/hello.v
@@ -0,0 +1,26 @@
+Inductive day : Type :=
+  | monday
+  | tuesday
+  | wednesday
+  | thursday
+  | friday
+  | saturday
+  | sunday.
+
+Definition next_working_day (d:day) : day :=
+  match d with
+  | monday => tuesday
+  | tuesday => wednesday
+  | wednesday => thursday
+  | thursday => friday
+  | friday => monday
+  | saturday => monday
+  | sunday => monday
+  end.
+
+Compute (next_working_day saturday).
+
+Example test_next_working_day:
+  (next_working_day (next_working_day saturday)) = tuesday.
+Proof. simpl. reflexivity. Qed.
+
