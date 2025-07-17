931318ae110954aa739e09837d7385076cece8a6
Luis Ángel Méndez Gort <lamg@protonmail.com>
2025-07-17 16:45:17 +0200

    Rocq is now a project

---
+++ rocq/.Makefile.d
@@ -0,0 +1,6 @@
+Basics.vo Basics.glob Basics.v.beautified Basics.required_vo: Basics.v 
+Basics.vio: Basics.v 
+Basics.vos Basics.vok Basics.required_vos: Basics.v 
+Induction.vo Induction.glob Induction.v.beautified Induction.required_vo: Induction.v Basics.vo
+Induction.vio: Induction.v Basics.vio
+Induction.vos Induction.vok Induction.required_vos: Induction.v Basics.vos
