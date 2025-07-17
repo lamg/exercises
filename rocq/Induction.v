97b7c2277a960125a45a21114f090f5102c38067
Luis Ángel Méndez Gort <lamg@protonmail.com>
2025-07-17 17:12:15 +0200

    first induction proof

--- rocq/Induction.v
+++ rocq/Induction.v
@@ -1,1 +1,15 @@
+
 From LF Require Export Basics.
+
+Import NatPlayground.
+
+Theorem add_0_r_firsttry : 
+  forall n:nat,
+  n + zero = n.
+  Proof.
+    intros n.
+    induction n as [|n ind].
+    - reflexivity.
+    - simpl. rewrite ind. reflexivity.
+  Qed.
+
