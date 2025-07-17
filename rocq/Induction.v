c3ceb2c663412bdf64df8a3556a7c86f4a3c0588
Luis Ángel Méndez Gort <lamg@protonmail.com>
2025-07-17 18:03:33 +0200

    add_assoc, add_commutativity, plus_n_succ_m, mul_zero_right, minus_n_n

--- rocq/Induction.v
+++ rocq/Induction.v
@@ -2,13 +2,62 @@ From LF Require Export Basics.
 
 Import NatPlayground.
 
-Theorem add_0_r_firsttry : 
+Theorem add_zero_right : 
   forall n:nat,
   n + zero = n.
   Proof.
     intros n.
-    induction n as [|n ind].
+    induction n as [|n' ind].
     - reflexivity.
+    - simpl. rewrite ind. reflexivity.
+  Qed.
+
+Theorem minus_n_n :
+  forall n, minus n n = zero.
+  Proof.
+  intros n. induction n as [| n' ind].
+  - simpl. reflexivity.
+  - simpl. rewrite ind. reflexivity. 
+  Qed.
+
+Theorem mul_zero_right: 
+  forall n:nat,
+  n * zero = zero.
+  Proof.
+    intros n.
+    induction n as [|n' ind].
+    - reflexivity.
+    - simpl. rewrite ind. reflexivity.
+  Qed.
+
+Theorem plus_n_succ_m : 
+  forall n m : nat,
+  succ (n + m) = n + (succ m).
+  Proof.
+    intros n m.
+    induction n as [|n' ind].
+    - reflexivity.
+    - simpl. rewrite ind. reflexivity.
+  Qed.
+
+Theorem add_commutativity : 
+  forall n m : nat,
+  n + m = m + n.
+  Proof.
+    intros n m.
+    induction n as [|n' ind].
+    - rewrite add_zero_right. reflexivity.
+    - simpl. rewrite ind. rewrite plus_n_succ_m. reflexivity.
+  Qed.
+
+
+Theorem add_assoc : 
+  forall n m p : nat,
+  n + (m + p) = (n + m) + p.
+  Proof.
+    intros n m p.
+    induction n as [|n' ind].
+    - simpl. reflexivity.
     - simpl. rewrite ind. reflexivity.
   Qed.
 
