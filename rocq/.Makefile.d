60fcf6ba386d7fab846cd7a35d8906461edb2a1a
Luis Ángel Méndez Gort <lamg@protonmail.com>
2025-08-03 15:26:07 +0200

    works with Proof General

--- rocq/.Makefile.d
+++ rocq/.Makefile.d
@@ -1,6 +1,9 @@
-Basics.vo Basics.glob Basics.v.beautified Basics.required_vo: Basics.v 
-Basics.vio: Basics.v 
-Basics.vos Basics.vok Basics.required_vos: Basics.v 
-Induction.vo Induction.glob Induction.v.beautified Induction.required_vo: Induction.v Basics.vo
-Induction.vio: Induction.v Basics.vio
-Induction.vos Induction.vok Induction.required_vos: Induction.v Basics.vos
+src/Basics.vo src/Basics.glob src/Basics.v.beautified src/Basics.required_vo: src/Basics.v 
+src/Basics.vio: src/Basics.v 
+src/Basics.vos src/Basics.vok src/Basics.required_vos: src/Basics.v 
+src/Induction.vo src/Induction.glob src/Induction.v.beautified src/Induction.required_vo: src/Induction.v src/Basics.vo
+src/Induction.vio: src/Induction.v src/Basics.vio
+src/Induction.vos src/Induction.vok src/Induction.required_vos: src/Induction.v src/Basics.vos
+test/Rocq_test.vo test/Rocq_test.glob test/Rocq_test.v.beautified test/Rocq_test.required_vo: test/Rocq_test.v 
+test/Rocq_test.vio: test/Rocq_test.v 
+test/Rocq_test.vos test/Rocq_test.vok test/Rocq_test.required_vos: test/Rocq_test.v 
