99e4fca8ad134b70015700c1fcb3020e1b2656e6
Luis Ángel Méndez Gort <lamg@protonmail.com>
2025-08-14 19:29:33 +0200

    or_commutativity

--- rocq/src/Logic.v
+++ rocq/src/Logic.v
@@ -49,3 +49,109 @@ Proof.
     + apply eq.
   - discriminate eq.
 Qed.
+
+Lemma and_example2:
+  forall n m : nat,
+    n = 0 /\ m = 0 -> n + m = 0.
+Proof.
+  intros n m H.
+  destruct H as [c0 c1].
+  rewrite c0.
+  rewrite c1.
+  reflexivity.
+Qed.
+
+Lemma and_example2'':
+  forall n m: nat,
+    n = 0 -> m = 0 -> n + m = 0.
+Proof.
+  intros n m h0 h1.
+  rewrite h0.
+  rewrite h1.
+  reflexivity.
+Qed.
+
+Lemma proj1:
+  forall p q : Prop, p /\ q -> p.
+Proof.
+  intros p q h.
+  destruct h as [h0 _].
+  apply h0.
+Qed.
+
+Lemma proj2:
+  forall p q : Prop, p /\ q -> q.
+Proof.
+  intros p q h.
+  destruct h as [_ h1].
+  apply h1.
+Qed.
+
+Theorem and_commutativity:
+  forall p q: Prop, p /\ q -> q /\ p.
+Proof.
+  intros p q [hp hq].
+  split.
+  - apply hq.
+  - apply hp.
+Qed.
+
+Theorem and_assoc:
+  forall p q r: Prop,
+    p /\ (q /\ r) -> (p /\ q) /\ r.
+Proof.
+  intros p q r [hp [hq hr]].
+  split.
+  - split.
+    + apply hp.
+    + apply hq.
+  - apply hr.
+Qed.
+
+Lemma factor_is_0:
+  forall n m : nat,
+    n = 0 \/ m = 0 -> n * m = 0.
+Proof.
+  intros n m [hn | hm].
+  - rewrite hn.
+    reflexivity.
+  - rewrite hm.
+    apply NatPlayground.mult_n_0.
+Qed.
+
+Lemma or_intro_l:
+  forall p q: Prop, p -> p \/ q.
+Proof.
+  intros p q hp.
+  left.
+  apply hp.
+Qed.
+
+Lemma zero_or_succ:
+  forall n: nat, n = 0 \/ n = S (pred n).
+Proof.
+  intros [|n'].
+  - left. reflexivity.
+  - right. reflexivity.
+Qed.
+
+Lemma mult_is_0:
+  forall n m, n * m = 0 -> n = 0 \/ m = 0.
+Proof.
+  intros n m h.
+  destruct n eqn:E.
+  - left. reflexivity.
+  - right.
+    simpl in h.
+    apply plus_is_0 in h.
+    apply proj1 in h.
+    apply h.
+Qed.
+
+Lemma or_commutativity:
+  forall p q: Prop, p \/ q -> q \/ p.
+Proof.
+  intros p q [hp | hq].
+  - right. apply hp.
+  - left. apply hq.
+Qed.
